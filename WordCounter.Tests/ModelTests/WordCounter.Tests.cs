556f8d13622a667c4086ee3e15b6ccbe5c85eaec
Alex Skreen <skreenalex@gmail.com>
2020-03-06 09:05:15 -0800

    add first test. Got meaningful fail

---
+++ WordCounter.Tests/ModelTests/WordCounter.Tests.cs
@@ -0,0 +1,17 @@
+ using Microsoft.VisualStudio.TestTools.UnitTesting;
+ using WordCounter.Models;
+
+ namespace WordCounter.Test
+ {
+    [TestClass]
+    public class WordCounterTests
+    {
+        [TestMethod]
+        public void ToUpperCase_TestCat_ReturnCAT()
+        {
+            UserInput testUserInput = new UserInput("cat", "cats are awesome");
+            Assert.AreEqual("Cat", testUserInput.ToUpperCase("cat"));
+
+        }
+    } 
+ }
