117df362211709043807f3ece5124dcc0b93336a
Alex Skreen <skreenalex@gmail.com>
2020-03-06 09:05:38 -0800

    add minimal code to run first test

---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,27 @@
+  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WordCounter.Models
+{
+    public class UserInput
+    {
+      public string Word { get; set;}
+      public string Sentence { get; set;}
+      public int occurance { get; set;}
+
+      public UserInput(string word, string Sentence)
+      {
+        Word = word;
+        Sentence = Sentence;
+      }
+
+        public string ToUpperCase(string Word)
+        {
+        string upperWord = Word.ToUpper();
+        return upperWord;
+        }
+      }
+    }
